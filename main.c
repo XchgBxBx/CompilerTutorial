b626cf36fbb0da83b23803088d898ecdf5603cbd
XchgBxBx <xchgbxbx@protonmail.com>
2017-08-17 09:15:08 -0300

    Expressions

--- main.c
+++ main.c
@@ -9,14 +9,26 @@ char look;
 
 void init();
 void nextChar();
+
 void error(char *fmt, ...);
 void fatal(char *fmt, ...);
 void expected(char *fmt, ...);
 void match(char c);
 char getName();
 char getNum();
+
 void emit(char *fmt, ...);
 
+void expression();
+void term();
+void factor();
+
+int isAddOp(char c);
+
+void add();
+void subtract();
+void multiply();
+void divide();
 
 void init()
 {
@@ -118,8 +130,107 @@ void emit(char *fmt, ...)
 	putchar('\n');
 }
 
+int isAddOp(char c)
+{
+	return (c == '+' || c == '-');
+}
+
+void add()
+{
+	match('+');
+	term();
+	emit("POP BX");
+	emit("ADD AX,BX");
+}
+
+void subtract()
+{
+	match('-');
+	term();
+	emit("POP BX");
+	emit("SUB AX, BX");
+	emit("NEG AX");
+}
+
+void multiply()
+{
+	match('*');
+	factor();
+	emit("POP BX");
+	emit("IMUL BX");
+}
+
+void divide()
+{
+	match('/');
+	factor();
+	emit("POP BX");
+	emit("XCHG AX, BX");
+	emit("CWD");
+	emit("IDIV BX");
+}
+
+void factor()
+{
+	if(look == '(')
+	{
+		match('(');
+		expression();
+		match(')');
+	}
+	else
+		emit("MOV AX, %c", getNum());
+}
+
+void term()
+{
+	factor();
+	while(look == '*' || look == '/')
+	{
+		emit("PUSH AX");
+		switch(look)
+		{
+		case '*':
+			multiply();
+			break;
+		case '/':
+			divide();
+			break;
+		default:
+			expected("MulOp");
+			break;
+		}
+	}
+}
+
+void expression()
+{
+	if(isAddOp(look))
+		emit("XOR AX, AX");
+	else
+		term();
+	while(isAddOp(look))
+	{
+		emit("PUSH AX");
+		switch(look)
+		{
+		case '+':
+			add();
+			break;
+		case '-':
+			subtract();
+			break;
+		default:
+			expected("AddOp");
+			break;
+		}
+	}
+}
+
 int main(int argc, char **argv)
 {
 	init();
+	expression();
+
 	return 0;
 }
